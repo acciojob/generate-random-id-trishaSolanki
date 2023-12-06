4c7abefff26eeed700716b11132328f9f7df4da0
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-06 14:56:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYXabcdefghijklmnopqrstuvwxyz0123456789';
+	const result = '';
+	const charactersLength = characters.length;
+	for(i=0; i<l; i++){
+		const randomIndex = Math.floor(MAth.random() * charactersLength);
+		result += characters.charAt(randomIndex);
+	}
+	return result;
 }
 
 // Do not change the code below.
