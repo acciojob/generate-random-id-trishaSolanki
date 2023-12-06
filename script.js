2c6883239384662d2efd78e4d9acdc8a39fa8b4d
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-06 15:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function makeid(l) {
-  // write your code here
-	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYXabcdefghijklmnopqrstuvwxyz0123456789';
-	const result = '';
-	const charactersLength = characters.length;
-	for(i=0; i<l; i++){
-		const randomIndex = Math.floor(Math.random() * charactersLength);
-		result += characters.charAt(randomIndex);
-	}
-	return result;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = ''; // Use let instead of const
+    const charactersLength = characters.length;
+
+    for(let i = 0; i < l; i++){ // Declare i with let for loop control
+        const randomIndex = Math.floor(Math.random() * charactersLength);
+        result += characters.charAt(randomIndex);
+    }
+
+    return result;
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
-
